ed0b07c4f176b39208f18c08ee61f4c86c6182d9
Nguyen Da <thanhda10cl@gmail.com>
2025-06-11 20:14:34 +0700

    change comment

--- AgileInspect/Code/Settings/GlobalSettings.cs
+++ AgileInspect/Code/Settings/GlobalSettings.cs
@@ -2,7 +2,7 @@
 {
     public static class GlobalSettings
     {
-        // RFC 2822 –  Tue, 10 Jun 2025 12:34:56 +0000
+        // RFC 2822 –  Tue, 10 Jun 2025 12:34:56 ZZ
         public const string FORMAT_RFC2822 = "ddd, dd MMM yyyy HH:mm:ss ZZ";
 
         // ISO 8601 –   2025-06-10T12:34:56Z
