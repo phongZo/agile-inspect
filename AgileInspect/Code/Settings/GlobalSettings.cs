c28ca03458aecf47cc54910bbce365f398d50967
Nguyen Da <thanhda10cl@gmail.com>
2025-06-11 20:13:47 +0700

    fix format datetime

--- AgileInspect/Code/Settings/GlobalSettings.cs
+++ AgileInspect/Code/Settings/GlobalSettings.cs
@@ -3,10 +3,10 @@
     public static class GlobalSettings
     {
         // RFC 2822 –  Tue, 10 Jun 2025 12:34:56 +0000
-        public const string FORMAT_RFC2822 = "ddd, DD MMM YYYY HH:mm:ss ZZ";
+        public const string FORMAT_RFC2822 = "ddd, dd MMM yyyy HH:mm:ss ZZ";
 
         // ISO 8601 –   2025-06-10T12:34:56Z
-        public const string FORMAT_ISO8601 = "YYYY-MM-DDTHH:mm:ssZ";
+        public const string FORMAT_ISO8601 = "yyyy-MM-ddTHH:mm:ssZ";
 
         public const string SecretKey = "AgileN@W";
     }
