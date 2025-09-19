0cfcb51c8203e113a439077d80d8c09635c61786
huypham <huyphuocbinh204@gmail.com>
2025-09-19 13:01:58 +0700

    add MultipleMonitors

---
+++ AgileInspect/Code/MonitorManagement/MultipleMonitors.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Drawing;
+
+namespace AgileInspect.Code.MonitorManagement
+{
+    public class MultipleMonitors
+    {
+        public static List<(Rectangle bounds, string deviceName)> GetMonitors()
+        {
+            var screens = new List<(Rectangle, string)>();
+
+            MonitorEnumProc callback = (IntPtr hMonitor, IntPtr hdcMonitor, ref RECT lprcMonitor, IntPtr dwData) =>
+            {
+                MONITORINFOEX mi = new MONITORINFOEX();
+                mi.cbSize = Marshal.SizeOf(typeof(MONITORINFOEX));
+
+                if (GetMonitorInfo(hMonitor, ref mi))
+                {
+                    int width = mi.rcMonitor.Right - mi.rcMonitor.Left;
+                    int height = mi.rcMonitor.Bottom - mi.rcMonitor.Top;
+                    Rectangle bounds = new Rectangle(mi.rcMonitor.Left, mi.rcMonitor.Top, width, height);
+                    screens.Add((bounds, mi.szDevice));
+                }
+
+                return true;
+            };
+
+            EnumDisplayMonitors(IntPtr.Zero, IntPtr.Zero, callback, IntPtr.Zero);
+            return screens;
+        }
+
+        private delegate bool MonitorEnumProc(IntPtr hMonitor, IntPtr hdcMonitor, ref RECT lprcMonitor, IntPtr dwData);
+        [DllImport("user32.dll")]
+        private static extern bool EnumDisplayMonitors(IntPtr hdc, IntPtr lprcClip, MonitorEnumProc lpfnEnum, IntPtr dwData);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern bool GetMonitorInfo(IntPtr hMonitor, ref MONITORINFOEX lpmi);
+        [StructLayout(LayoutKind.Sequential)]
+        struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        struct MONITORINFOEX
+        {
+            public int cbSize;
+            public RECT rcMonitor;
+            public RECT rcWork;
+            public uint dwFlags;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string szDevice;
+        }
+    }
+}
--- WatermarkDetectorPlugin/Code/WatermarkDetector.cs
+++ WatermarkDetectorPlugin/Code/WatermarkDetector.cs
@@ -1,9 +1,9 @@
 ﻿using AgileInspect.Code.Rules;
 using AgileInspect.Code.Settings;
+using AgileInspect.Code.MonitorManagement;
 using Compunet.YoloSharp;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.Runtime.InteropServices;
 using System.Text.Json;
 
 namespace WatermarkDetectorPlugin
@@ -47,7 +47,7 @@ namespace WatermarkDetectorPlugin
             string eventType = StoreCfgLoader.mapPluginNameToEventType(pluginName);
             bool isOn = true;
 
-            var screens = GetMonitors();
+            var screens = MultipleMonitors.GetMonitors();
             foreach (var screen in screens)
             {
                 using (Bitmap bmp = GetBitmapFromScreen(screen.bounds))
@@ -73,30 +73,6 @@ namespace WatermarkDetectorPlugin
             };
             string jsonResult = JsonSerializer.Serialize(resultObj);
             PluginContext.SendDetectionResult(pluginName, jsonResult);
-        }
-
-        private List<(Rectangle bounds, string deviceName)> GetMonitors()
-        {
-            var screens = new List<(Rectangle, string)>();
-
-            MonitorEnumProc callback = (IntPtr hMonitor, IntPtr hdcMonitor, ref RECT lprcMonitor, IntPtr dwData) =>
-            {
-                MONITORINFOEX mi = new MONITORINFOEX();
-                mi.cbSize = Marshal.SizeOf(typeof(MONITORINFOEX));
-
-                if (GetMonitorInfo(hMonitor, ref mi))
-                {
-                    int width = mi.rcMonitor.Right - mi.rcMonitor.Left;
-                    int height = mi.rcMonitor.Bottom - mi.rcMonitor.Top;
-                    Rectangle bounds = new Rectangle(mi.rcMonitor.Left, mi.rcMonitor.Top, width, height);
-                    screens.Add((bounds, mi.szDevice));
-                }
-
-                return true;
-            };
-
-            EnumDisplayMonitors(IntPtr.Zero, IntPtr.Zero, callback, IntPtr.Zero);
-            return screens;
         }
 
         private async Task<bool> DetectWatermarkAsync(Bitmap bitmap, string deviceName)
@@ -132,32 +108,6 @@ namespace WatermarkDetectorPlugin
             using var ms = new MemoryStream();
             bitmap.Save(ms, ImageFormat.Png);
             return ms.ToArray();
-        }
-
-        private delegate bool MonitorEnumProc(IntPtr hMonitor, IntPtr hdcMonitor, ref RECT lprcMonitor, IntPtr dwData);
-        [DllImport("user32.dll")]
-        private static extern bool EnumDisplayMonitors(IntPtr hdc, IntPtr lprcClip, MonitorEnumProc lpfnEnum, IntPtr dwData);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        static extern bool GetMonitorInfo(IntPtr hMonitor, ref MONITORINFOEX lpmi);
-        [StructLayout(LayoutKind.Sequential)]
-        struct RECT
-        {
-            public int Left;
-            public int Top;
-            public int Right;
-            public int Bottom;
-        }
-
-        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
-        struct MONITORINFOEX
-        {
-            public int cbSize;
-            public RECT rcMonitor;
-            public RECT rcWork;
-            public uint dwFlags;
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
-            public string szDevice;
         }
     }
 }
